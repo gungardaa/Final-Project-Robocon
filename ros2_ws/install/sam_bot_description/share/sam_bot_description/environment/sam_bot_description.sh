086a7f901a5e64c51705d0f530d344de4e185463
Arda Nareswara <agungarda81@gmail.com>
2024-11-17 18:09:56 +0700

    Commit

---
+++ ros2_ws/install/sam_bot_description/share/sam_bot_description/environment/sam_bot_description.sh
@@ -0,0 +1,3 @@
+
+ament_prepend_unique_value GAZEBO_MODEL_PATH "$AMENT_CURRENT_PREFIX/share/sam_bot_description/models"
+ament_prepend_unique_value GAZEBO_RESOURCE_PATH "$AMENT_CURRENT_PREFIX/share/sam_bot_description/worlds"
